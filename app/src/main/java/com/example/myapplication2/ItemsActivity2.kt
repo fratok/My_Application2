3108235291d82dd08193c7bc21a9c6f99c21adc3
fratok <fratia777@gmail.com>
2024-09-22 22:07:46 +0300

    `111`

--- app/src/main/java/com/example/myapplication2/ItemsActivity2.kt
+++ app/src/main/java/com/example/myapplication2/ItemsActivity2.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import org.json.JSONArray
 import org.json.JSONObject
 
@@ -25,23 +26,8 @@ class   ItemsActivity2 : AppCompatActivity() {
         val jsonString = resources.openRawResource(R.raw.shopping_list).bufferedReader().use { it.readText() }
         val jsonArray = JSONArray(jsonString)
 
-//        for (i in 0 until jsonArray.length()) {
-//            val jsonObject = jsonArray.getJSONObject(i)
-//            val item = Item(
-//                id = jsonObject.getInt("id"),
-//                image = jsonObject.getString("image"),
-//                name = jsonObject.getString("name"),
-//                description = jsonObject.getString("description"),
-//                jsonObject.getString("additionalInfo1"),
-//                jsonObject.getString("additionalInfo2"),
-//                jsonObject.getString("additionalInfo3"),
-//                price = jsonObject.getInt("price")
-//            )
-//            items.add(item)
-//        }
         val builder = GsonBuilder().create()
-//        val ggson = builder.fromJson(jsonString, ArrayList<Item>()::class.java)
-        val ggson = builder.fromJson(jsonString, MutableList<Item>::class.java)
+        val ggson = Gson().fromJson(jsonString, object : TypeToken<List<Item>>() {}.type)
         val itemsAdapter = ItemsAdapter(ggson, context = this)
 
         itemsList.layoutManager = LinearLayoutManager(this)
