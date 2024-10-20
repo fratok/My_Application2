708e86aa942fe121c799ed6e4ec1fcf6aa1c1fb7
fratok <fratia777@gmail.com>
2024-10-20 14:17:51 +0300

    добавил запрос

--- app/src/main/java/com/example/myapplication2/Item.kt
+++ app/src/main/java/com/example/myapplication2/Item.kt
@@ -5,6 +5,12 @@ import kotlinx.serialization.Serializable
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Call
+import okhttp3.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
 @Serializable
 data class Item(
     val id: Int,
@@ -21,3 +27,12 @@ data class ItemList(
     val list: List<Item>
 )
 
+interface ItemApi{
+    @GET("/JSON/shopping_list.json")
+    fun getItem(): Call<ItemList>
+}
+val retrofit = Retrofit.Builder()
+    .baseUrl("https://fratok.github.io/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+val itemApi: ItemApi = retrofit.create(ItemApi::class.java)
